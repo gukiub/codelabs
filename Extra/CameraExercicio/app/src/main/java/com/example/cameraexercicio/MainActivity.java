9558847f5ed254c5764810ff3d606c0fc8c779b0
gukiub <gu_kiub@hotmail.com>
2020-04-22 11:45:26 -0300

    exercicio_camera

---
+++ Extra/CameraExercicio/app/src/main/java/com/example/cameraexercicio/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.cameraexercicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUISITAR_CAPTURA_DE_IMAGEM = 1;
+    private ImageView foto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        foto = findViewById(R.id.foto);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUISITAR_CAPTURA_DE_IMAGEM && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            foto.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    public void tirarFoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUISITAR_CAPTURA_DE_IMAGEM);
+        }
+    }
+}
